a19b84523b518be023820f1e76103c7018e9e8f9
besong-marlon <besongmarlon014@gmail.com>
2025-07-25 21:35:37 +0100

    final

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburgerMenu = document.getElementById("hamburgerMenu");
-const navMenu = document.getElementById("callToAction");
-
-hamburgerMenu.addEventListener("click", () => {
-  hamburgerMenu.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
